ee20fa293db7d2b7ab213019ac9ecb477c73e589
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2018-09-30 20:24:14 -0600

    Added Array of quotes and then added preliminary Get Random Quote function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
+var quotes = [
+  quote_1 {
+    quote:"Don't cry because it's over, smile because it happened.";
+    source: "Dr.Seuss";
+  }
+  quote_2 {
+    quote:“The unexamined life is not worth living”;
+    source:"Socrates";
+  }
+  quote_3 {
+    quote:“One cannot step twice in the same river”;
+    source:"Heraclitus"
+  }
+  quote_4 {
+    quote:“There is only one good, knowledge, and one evil, ignorance”;
+    source:"Socrates";
+  }
+  quote_5 {
+    quote:“We are what we repeatedly do. Excellence, then, is not an act, but a habit”;
+    source:"Aristotle";
+  }
+]
 
+
+// Create the getRandomQuuote function and name it getRandomQuote
+getRandomQuote (array) {
+ return array[math.random()];
+}
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
