574a0e18662699078523096064dd1b11dff53762
Edmund Yu <eyu1@ualberta.ca>
2018-10-01 19:20:08 -0600

    Working cont.
    -Fixed Array syntax
    - Sucessfully implemented getRandomQuote function

--- js/script.js
+++ js/script.js
@@ -2,34 +2,40 @@
 
 // Create the array of quote objects and name it quotes
 var quotes = [
-  quote_1 {
-    quote:"Don't cry because it's over, smile because it happened.";
-    source: "Dr.Seuss";
-  }
-  quote_2 {
-    quote:“The unexamined life is not worth living”;
-    source:"Socrates";
-  }
-  quote_3 {
-    quote:“One cannot step twice in the same river”;
+  {
+    quote:"Don't cry because it's over, smile because it happened.",
+    source: "Dr.Seuss"
+  },
+  {
+    quote: "The unexamined life is not worth living.",
+    source:"Socrates",
+    citation:"According to Plato's Apology(470-399 BC)",
+    year:"470-399 BC"
+  },
+  {
+    quote:"One cannot step twice in the same river",
     source:"Heraclitus"
+  },
+  {
+    quote:"There is only one good, knowledge, and one evil, ignorance",
+    source:"Socrates"
+  },
+  {
+    quote:"We are what we repeatedly do. Excellence, then, is not an act, but a habit",
+    source:"Aristotle"
   }
-  quote_4 {
-    quote:“There is only one good, knowledge, and one evil, ignorance”;
-    source:"Socrates";
-  }
-  quote_5 {
-    quote:“We are what we repeatedly do. Excellence, then, is not an act, but a habit”;
-    source:"Aristotle";
-  }
-]
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
-getRandomQuote (array) {
- return array[math.random()];
+function getRandomQuote (array) {
+  var random_num = Math.floor(Math.random() * array.length);  // Get a random number between 0-array.length, since array starts at index 0.
+  var random_quote = array[random_num];
+  return random_quote.quote
 }
-
+// Test code
+//var test = getRandomQuote(quotes);
+//console.log(test);
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
